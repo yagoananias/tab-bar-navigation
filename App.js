cf2e413e88d019eb4c1d2dba0905b0fb23d8ac6b
Yago Ananias <yago_souza92@yahoo.com.br>
2020-05-25 22:16:41 -0300

    Finalizando Tab Bar

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
+import { FontAwesome5 } from '@expo/vector-icons'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import { JournalScreen, MeasuresScreen, TreatmentScreen, ProfileScreen } from './screens/'
+import AddButton from './components/AddButton'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const TabNavigator = createBottomTabNavigator({
+    Journal: {
+      screen: JournalScreen,
+      navigationOptions: {
+        tabBarIcon: () => <FontAwesome5 name="book-medical" size={24} color='#926bbb' />
+      }
+    },
+    Measures: {
+      screen: MeasuresScreen,
+      navigationOptions: {
+        tabBarIcon: () => <FontAwesome5 name="heartbeat" size={24} color='#926bbb' />
+      }
+    },
+    Add: {
+      screen: () => null,
+      navigationOptions: {
+        tabBarIcon: <AddButton />
+      }
+    },
+    Treatment: {
+      screen: TreatmentScreen,
+      navigationOptions: {
+        tabBarIcon: () => <FontAwesome5 name="band-aid" size={24} color='#926bbb' />
+      }
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        tabBarIcon: () => <FontAwesome5 name="user" size={24} color='#926bbb' />
+      }
+    }
+},
+
+ {  
+   tabBarOptions: {
+     showLabel: false
+   }
+ }
+)
+export default createAppContainer(TabNavigator)
